c9bd1832a65be9f673063784d17b8bee426456bd
Andrew Moshkovich <amoshkov@ucsc.edu>
2022-04-04 22:58:51 -0700

    main.py modified for Linux

---
+++ main.py
@@ -0,0 +1,95 @@
+import time
+from selenium.webdriver.chrome.service import Service
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import pandas as pd
+import pyperclip
+import regex
+
+#sample CIK codes to paste into the input window
+#0000063908, 0000320193, 0000038009
+
+print("PLease enter the CIK Number or Ticker of every company you would like to search, separated by commas")
+companies = input('>')
+companies = companies.replace(" ", "")
+companies = companies.split(",")
+print("retrieving files")
+
+driver = webdriver.Chrome(executable_path="/usr/local/bin/chromedriver/chromedriver")
+driver.implicitly_wait(1)
+
+url = "https://www.sec.gov/edgar/searchedgar/companysearch.html"
+
+filing_records = pd.DataFrame(columns=['form_type_description',
+                                       'filing_date',
+                                       'reporting_date',
+                                       'company_url',
+                                       'company_cik'])
+
+for company in companies:
+    driver.get(url)
+
+    driver.find_element(By.XPATH, "//input[@id='company']").send_keys(company)
+
+    time.sleep(.1)
+
+    try:
+        driver.find_element(By.LINK_TEXT, "No thanks").click()
+        driver.find_element(By.XPATH, "//input[@id='company']").clear()
+        driver.find_element(By.XPATH, "//input[@id='company']").send_keys(company)
+    except:
+        pass
+
+    driver.find_element(By.XPATH, "//table[@class ='smart-search-entity-hints']").click()
+
+    driver.find_element(By.XPATH, "//button[@id='btnViewAllFilings']").click()
+
+    driver.find_element(By.XPATH, "//input[@id='filingDateFrom']").clear()
+    driver.find_element(By.XPATH, "//input[@id='filingDateFrom']").send_keys("2000-01-01")
+    driver.find_element(By.XPATH, "//input[@id='filingDateFrom']").send_keys(Keys.ENTER)
+
+    driver.find_element(By.XPATH, "//span[normalize-space()='Copy to clipboard']").click()
+
+    df = pyperclip.paste()
+    df = regex.sub(",", "", df)
+    df = df.replace("EDGAR Entity Landing Page", "")
+    df = regex.sub("Form type\s*Form description\s*Filing date\s*Reporting date\s*Filings URL",
+                   "form_type_description, filing_date,	reporting_date,	file_link",
+                   df)
+    df = regex.sub("\n", "", df, 2)
+    df = regex.sub("(?=\D[0-9]{4}-[0-9]{2}-[0-9]{2})", ",", df)
+    df = regex.sub("(?<=[0-9]{4}-[0-9]{2}-[0-9]{2}\s{2})", ",", df)
+    df = regex.sub("(?=https)", ",", df)
+    df = regex.sub("(?<=[0-9]),\s*,(?=[0-9])", ",", df)
+    df = regex.sub("(?<=[0-9]),\s,(?=h)", ",", df)
+
+    df = df.split("\n")
+    df = pd.DataFrame([row.split(",") for row in df])
+    df.columns = ['form_type_description',
+                  'filing_date',
+                  'reporting_date',
+                  'company_url']
+    df = df.drop([0])
+    df['company_cik'] = [company]*len(df.index)
+
+    filing_records = pd.concat([filing_records, df])
+
+filing_records.to_csv("posts/filings.csv")
+
+#### alternate code to just download a csv for each company (downside: many csv's)
+# for company in companies:
+#     driver.get(url)
+#
+#     driver.find_element(By.XPATH, "//input[@id='company']").send_keys(company)
+#
+#     time.sleep(.5)
+#     driver.find_element(By.XPATH, "//table[@class ='smart-search-entity-hints']").click()
+#
+#     driver.find_element(By.XPATH, "//button[@id='btnViewAllFilings']").click()
+#     time.sleep(.5)
+#     driver.find_element(By.XPATH, "//input[@id='filingDateFrom']").clear()
+#     driver.find_element(By.XPATH, "//input[@id='filingDateFrom']").send_keys("2000-01-01")
+#     time.sleep(.5)
+#     driver.find_element(By.XPATH, "//span[normalize-space()='CSV']").click()
+
